813ff57af5ff3d2be645f816788597b96605eb3e
alishaestyy <alishaesteves27@gmail.com>
2022-12-13 06:33:59 -0500

    testing changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,8 @@
+"""docstring"""
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,24 +10,22 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-authenticator = IAMAuthenticator('{apikey}')
+authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
-    version='{version}',
+    version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('{url}')
+language_translator.set_service_url(url)
 
-def englishToFrench(englishText):
-    translation = language_translator.translate(
-        text=englishText, 
-        model_id='en-fr').get_result()
-    frenchText=json.dumps(translation, indent=2, ensure_ascii=False)
-    return frenchText
+def english_to_french(english_text):
+    """translates english to french"""
+    translation = language_translator.translate(text=english_text, model_id='en-fr').get_result()
+    french_text=json.dumps(translation, indent=2, ensure_ascii=False)
+    return french_text
 
-def frenchToEnglish(frenchText):
-    translation = language_translator.translate(
-        text=frenchText, 
-        model_id='fr-en').get_result()
-    frenchText=json.dumps(translation, indent=2, ensure_ascii=False)
-    return englishText
+def french_to_english(french_text):
+    """translates french to english"""
+    translation = language_translator.translate(text=french_text, model_id='fr-en').get_result()
+    english_text=json.dumps(translation, indent=2, ensure_ascii=False)
+    return english_text
